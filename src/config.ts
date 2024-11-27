ca7c67711c38e0b94b1495408e6c17f22ea3b208
Ayushsinha2629 <ayush61187@gmail.com>
2024-11-28 03:28:54 +0530

    Refactor: Config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,2 @@
 export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL
-export const REACT_BASE_URL = import.meta.env.VITE_REACT_BASE_URL || "http://localhost:5173";
+export const REACT_BASE_URL = import.meta.env.VITE_REACT_BASE_URL || "https://second-brain-fe-two.vercel.app";
