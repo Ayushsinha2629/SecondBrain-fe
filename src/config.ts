9dbb43299ab42bd404eb9ca6de2c9b77fc1ed09c
Ayushsinha2629 <ayush61187@gmail.com>
2024-11-28 03:01:50 +0530

    Added  gitignore

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,2 @@
-export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "https://secondbrain-be-ja9p.onrender.com";
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL
 export const REACT_BASE_URL = import.meta.env.VITE_REACT_BASE_URL || "http://localhost:5173";
